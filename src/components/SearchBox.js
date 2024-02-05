5db2219d00a2640fc6dab2175c7b3fa12c59c77a
Elizab._.th <72495673+galambova@users.noreply.github.com>
2024-02-05 22:24:46 +0100

    updating colours and border radius of input

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,6 +4,7 @@ const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div>
       <input
+        style={{ borderRadius: 150, margin: 20 }}
         className="pa3 ba b--green bg-lightest-blue"
         type="search"
         placeholder="search robots"
